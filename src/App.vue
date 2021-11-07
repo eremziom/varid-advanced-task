1c8c01f78edafbc80d1fb1cce8e267834c9b9c54
Maciej Kryczka <maciej.kryczka@toya.com.pl>
2021-11-07 18:09:43 +0100

    create vue project, prepare components, install bootstrap

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h2>Varid Advanced Task</h2>
+    <Form />
+    <Table />
+  </div>
+</template>
+
+<script>
+
+import Table from './components/Table.vue';
+import Form from './components/Form.vue'
+export default {
+  name: 'App',
+  components: {
+    Table,
+    Form
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
